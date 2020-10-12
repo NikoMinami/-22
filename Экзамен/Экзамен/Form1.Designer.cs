be6fd41537921c0c0286050f29489e4d062f66d1
NikoMinami <71691097+NikoMinami@users.noreply.github.com>
2020-10-12 17:20:32 +0500

    Add files via upload

---
+++ Экзамен/Экзамен/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Экзамен
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTime = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.btnPrice = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTime.ForeColor = System.Drawing.Color.Black;
+            this.lblTime.Location = new System.Drawing.Point(12, 29);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(231, 17);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "Время вашего разговора,Минуты:";
+            // 
+            // txtTime
+            // 
+            this.txtTime.ForeColor = System.Drawing.Color.Black;
+            this.txtTime.Location = new System.Drawing.Point(247, 29);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(38, 20);
+            this.txtTime.TabIndex = 1;
+            this.txtTime.TextChanged += new System.EventHandler(this.txtTime_TextChanged);
+            this.txtTime.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTime_KeyPress);
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblDay.ForeColor = System.Drawing.Color.Black;
+            this.lblDay.Location = new System.Drawing.Point(12, 63);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(257, 17);
+            this.lblDay.TabIndex = 2;
+            this.lblDay.Text = "Номер дня недели вашего разговора:";
+            // 
+            // txtDay
+            // 
+            this.txtDay.ForeColor = System.Drawing.Color.Black;
+            this.txtDay.Location = new System.Drawing.Point(275, 63);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(38, 20);
+            this.txtDay.TabIndex = 3;
+            this.txtDay.TextChanged += new System.EventHandler(this.txtDay_TextChanged);
+            this.txtDay.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDay_KeyPress);
+            // 
+            // btnPrice
+            // 
+            this.btnPrice.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnPrice.ForeColor = System.Drawing.Color.Black;
+            this.btnPrice.Location = new System.Drawing.Point(12, 136);
+            this.btnPrice.Name = "btnPrice";
+            this.btnPrice.Size = new System.Drawing.Size(75, 38);
+            this.btnPrice.TabIndex = 4;
+            this.btnPrice.Text = "Расчет стоимости";
+            this.btnPrice.UseVisualStyleBackColor = false;
+            this.btnPrice.Click += new System.EventHandler(this.btnPrice_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Gold;
+            this.btnClear.ForeColor = System.Drawing.Color.Black;
+            this.btnClear.Location = new System.Drawing.Point(128, 136);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 38);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Сброс расчёта";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.ForeColor = System.Drawing.Color.Black;
+            this.btnExit.Location = new System.Drawing.Point(247, 136);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 38);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightPink;
+            this.ClientSize = new System.Drawing.Size(334, 186);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPrice);
+            this.Controls.Add(this.txtDay);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lblTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.TextBox txtDay;
+        private System.Windows.Forms.Button btnPrice;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Экзамен/Экзамен/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Экзамен
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrice_Click(object sender, EventArgs e)
+        {
+            int price = 6;
+            int time = Convert.ToInt32(txtTime.Text);
+            int day = Convert.ToInt32(txtDay.Text);
+
+            if (day <= 0 || day >= 8)
+            {
+                MessageBox.Show("Ошибка", "Сообщение");
+            }
+            if (day == 6 || day == 7)
+            {
+                double total = (time * price) - (time * price * 0.2);
+                MessageBox.Show("Длительность разговора " + time + " мин., Он произошёл в " + day + "-й день недели. Его Стоимость составила " + total + " руб.", "Сообщение");
+            }
+            if (day >= 1 && day <= 5)
+            {
+                int total = time * price;
+                MessageBox.Show("Длительность разговора" + time + " мин., Он произошёл в " + day + "-й день недели. Его Стоимость составила " + total + " руб.", "Сообщение");
+            }
+            txtDay.Clear();
+            txtTime.Clear();
+            btnPrice.Enabled = false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDay.Clear();
+            txtTime.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtTime_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (e.KeyChar <= 47 || e.KeyChar >= 58)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtTime_TextChanged(object sender, EventArgs e)
+        {
+            if (txtTime.Text.Length > 0 && txtDay.Text.Length > 0)
+            {
+                btnPrice.Enabled = true;
+            }
+        }
+
+        private void txtDay_TextChanged(object sender, EventArgs e)
+        {
+            if (txtDay.Text.Length > 0 && txtTime.Text.Length > 0)
+            {
+                btnPrice.Enabled = true;
+            }
+        }
+
+        private void txtDay_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (e.KeyChar <= 47 || e.KeyChar >= 58)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
